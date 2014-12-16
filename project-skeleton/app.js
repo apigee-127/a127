52c525ebdc47b6a54a0607b2fbb4b472d1ec07b0
Scott Ganyo <scott@ganyo.com>
2014-12-15 16:21:13 -0800

    print "127.0.0.1" instead of "localhost" to avoid IPv6 routing issue

--- project-skeleton/app.js
+++ project-skeleton/app.js
@@ -13,5 +13,5 @@ a127.init(function(config) {
 
   app.listen(process.env.PORT || 10010);
 
-  console.log('try this:\ncurl http://localhost:10010/hello?name=Scott');
+  console.log('try this:\ncurl http://127.0.0.1:10010/hello?name=Scott');
 });
