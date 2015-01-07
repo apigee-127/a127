52da3b9f718f63b1eef36356aae6c17e3dad10c6
Scott Ganyo <scott@ganyo.com>
2015-01-07 11:34:09 -0800

    add test command and example tests in skeleton

--- project-skeleton/api/controllers/hello_world.js
+++ project-skeleton/api/controllers/hello_world.js
@@ -36,8 +36,8 @@ module.exports = {
  */
 function hello(req, res) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var name = req.swagger.params.name.value;
-  var hello = name ? util.format('Hello, %s', name) : 'Hello, stranger!';
+  var name = req.swagger.params.name.value || 'stranger';
+  var hello = util.format('Hello, %s!', name);
 
   // this sends back a JSON response which is a single string
   res.json(hello);
